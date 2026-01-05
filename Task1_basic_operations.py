092fda8ddf68a62e51d9f79b6213707a06e53865
Dhanyashree S <dhanyashreejithesh@gmail.com>
2026-01-05 13:03:38 +0530

    Add files via upload

---
+++ Task1_basic_operations.py
@@ -0,0 +1,10 @@
+num1=float(input("enter first number"))
+num2=float(input("enter second number"))
+addition=num1+num2
+subtraction=num1-num2
+multiplication=num1*num2
+division=num1/num2
+print("addition is",addition)
+print("subtraction is",subtraction)
+print("multiplication is",multiplication)
+print("division is",division)
